89130e383a26b2cd8cb4fc69bdb11d226ec226e3
meghann <meghannfh@gmail.com>
2022-12-15 21:59:04 -0600

    i can't decide what to do with all this

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require('../models/Post')
+const User = require('../models/User')
 const multer = require('multer')
 
 module.exports = {
@@ -21,10 +22,9 @@ module.exports = {
     },
     getPost: async (req, res) => {
         try{
-            const artist = await Post.find({ user: req.user.id})
-            console.log(artist.userName)
             const post = await Post.findById(req.params.id);
-        res.render('post.ejs', { post: post, user: req.user })
+            const artist = await User.findById({ _id: post.user})
+        res.render('post.ejs', { post: post, user: req.user, artist: artist })
         }catch(err){
             console.error(err)
         }
--- public/js/showForm.js
+++ public/js/showForm.js
@@ -1,21 +1,19 @@
 function handleAddClick(){
   const btn = document.getElementById('add')
   const form = document.getElementById('form')
-  const editImgBtn = document.getElementById('edit-img-btn')
   const editImgBtns = document.querySelectorAll('.edit-img-btns')
   const editImgForm = document.getElementById('edit-profile-img-form')
 
   btn.addEventListener('click', () => {
     form.classList.toggle('down')
     if(form.classList.contains('down')){
-      btn.innerText = "hide"
+      btn.innerHTML = `<i class="fa-solid fa-minus"></i>`
     } else {
-      btn.innerText = "+"
+      btn.innerHTML = `<i class="fa-solid fa-plus"></i>`
     }
   })
 
-  editImgBtns.forEach(btn => btn.addEventListener('click', (e) => {
-    const clicked = e.target
+  editImgBtns.forEach(btn => btn.addEventListener('click', () => {
     editImgForm.classList.toggle('hide')
   }))
 }
